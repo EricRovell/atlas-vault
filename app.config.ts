6e9574b31b65160f70ba20444b8b243be729416e
EricRovell <ericrovell@gmail.com>
2024-12-24 00:35:06 +0300

    chore: update conf

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  vite: {
-    server: {
-      host: true,
-      port: 3000,
-      hmr: {
-        port: 4000
-      },
-      watch: {
-        usePolling: true,
-        interval: 1500
-      }
-    }
-  }
+	vite: {
+		server: {
+			host: true,
+			port: 3000
+		}
+	}
 });
